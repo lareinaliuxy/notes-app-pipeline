c0ed83a05a4b5963a920397b7a7851ac8420d7c7
lareinaliuxy <liu.xinyi2@husky.neu.edu>
2021-03-23 23:08:36 -0700

    use hook to generalize form fields handling

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,40 +1,42 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import LoaderButton from "../components/LoaderButton";
-import "./Login.css";
 import { Auth } from "aws-amplify";
-import { useAppContext } from "../libs/contextLib";
+import Form from "react-bootstrap/Form";
 import { useHistory } from "react-router-dom";
+import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../libs/contextLib";
+import { useFormFields } from "../libs/hooksLib";
 import { onError } from "../libs/errorLib";
-
+import "./Login.css";
 
 export default function Login() {
-    const [isLoading, setIsLoading] = useState(false);
-    const history = useHistory();
-    const { userHasAuthenticated } = useAppContext();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const history = useHistory();
+  const { userHasAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
 
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
+  function validateForm() {
+    return fields.email.length > 0 && fields.password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      await Auth.signIn(fields.email, fields.password);
+      userHasAuthenticated(true);
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
     }
+  }
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-
-        setIsLoading(true);
-
-        try {
-          await Auth.signIn(email, password);
-          userHasAuthenticated(true);
-          history.push("/");
-        } catch (e) {
-          onError(e);
-          setIsLoading(false);
-        }
-    }
-
-return (
+  return (
     <div className="Login">
       <Form onSubmit={handleSubmit}>
         <Form.Group size="lg" controlId="email">
@@ -42,16 +44,16 @@ return (
           <Form.Control
             autoFocus
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <LoaderButton
@@ -65,5 +67,5 @@ return (
         </LoaderButton>
       </Form>
     </div>
-);
+  );
 }
