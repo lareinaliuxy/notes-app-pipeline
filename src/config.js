c49a3bd4afd0e1d41db60e21f4df55c3cc0ed8c3
lareinaliuxy <liu.xinyi2@husky.neu.edu>
2021-03-23 21:18:47 -0700

    Setting up our React app, configuring aws amplify

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-xinyiliu",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: " https://kkkzkssc42.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_Y4VlikIIu",
+      APP_CLIENT_ID: "7khnnq08c3qobgbkic9pbto7m1",
+      IDENTITY_POOL_ID: "us-east-1:fa5024ba-0774-4c63-86e0-5eaac47de2e2",
+    },
+  };
+
+  export default config;
