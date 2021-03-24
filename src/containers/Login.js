30bb970ca9f7d4b3bf11d9e00aba7a072760a345
lareinaliuxy <liu.xinyi2@husky.neu.edu>
2021-03-23 22:54:54 -0700

    add feature to give feedback indicating login call in progress

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 import { Auth } from "aws-amplify";
 import { useAppContext } from "../libs/contextLib";
 import { useHistory } from "react-router-dom";
 
+
 export default function Login() {
+    const [isLoading, setIsLoading] = useState(false);
     const history = useHistory();
     const { userHasAuthenticated } = useAppContext();
     const [email, setEmail] = useState("");
@@ -19,12 +21,15 @@ export default function Login() {
     async function handleSubmit(event) {
         event.preventDefault();
 
+        setIsLoading(true);
+
         try {
           await Auth.signIn(email, password);
           userHasAuthenticated(true);
           history.push("/");
         } catch (e) {
           alert(e.message);
+          setIsLoading(false);
         }
     }
 
@@ -48,9 +53,15 @@ return (
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
+        <LoaderButton
+          block
+          size="lg"
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </Form>
     </div>
 );
