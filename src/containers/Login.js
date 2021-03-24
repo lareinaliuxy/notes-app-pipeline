a692babb1059e06ac53cca5c0c69c4eccd9c6166
lareinaliuxy <liu.xinyi2@husky.neu.edu>
2021-03-23 21:56:06 -0700

    add feature to reflect a logged in status, and creat a logout button

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
+import { Auth } from "aws-amplify";
+import { useAppContext } from "../libs/contextLib";
 
 export default function Login() {
+    const { userHasAuthenticated } = useAppContext();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -11,8 +14,15 @@ export default function Login() {
         return email.length > 0 && password.length > 0;
     }
 
-    function handleSubmit(event) {
+    async function handleSubmit(event) {
         event.preventDefault();
+
+        try {
+          await Auth.signIn(email, password);
+          userHasAuthenticated(true);
+        } catch (e) {
+          alert(e.message);
+        }
     }
 
 return (
